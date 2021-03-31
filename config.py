66880106bc79efa1b8e6e58eb18489713e66311a
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-03-31 14:49:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 
