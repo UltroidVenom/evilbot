433b05d595c8a8a7c645d71570884e81a3b48255
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-03-31 19:24:18 +0530

    Update repo.py

--- handlers/repo.py
+++ handlers/repo.py
@@ -7,7 +7,6 @@ from pyrogram import idle, filters
 from pyrogram.handlers import MessageHandler
 from helpers.wrappers import errors, admins_only
 from config import BOT_NAME as bn
-
 @Client.on_message(command("repo") & other_filters)
 @errors
 async def start(client: Client, message: Message):
