80fcfb6cd98010ba5e6a93aa487799b837b02d80
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-04-20 21:37:00 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -515,7 +515,7 @@ async def play(_, message: Message):
         await message.reply_photo(
         photo="final.png",
         reply_markup=keyboard,
-        caption="▶️ **Playing** here the song requested by {} via {bn} 😜".format(
+        caption="▶️ **Playing** here the song requested by {} via Evilmusicbot😜".format(
         message.from_user.mention()
         ),
     )
