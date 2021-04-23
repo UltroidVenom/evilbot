b16dbd5b38470681f7e98f17c4e29da95fc26b83
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-04-23 13:20:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ BOT_NAME = getenv("BOT_NAME")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
+admins = {}
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
