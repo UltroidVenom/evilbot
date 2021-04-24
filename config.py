8bb828b1c84ab569d2c7e2b2be8e6bfaef201bc0
Pawan JATT <76043107+jattpawan@users.noreply.github.com>
2021-04-24 21:20:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+
 from os import getenv
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,12 +10,12 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-
+admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-admins = {}
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
