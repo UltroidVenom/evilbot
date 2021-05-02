92dca5adb9f83d9298e19034c97d2d0f524a0f79
UltroidVenom <80195199+UltroidVenom@users.noreply.github.com>
2021-05-02 13:02:13 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -25,7 +25,7 @@ The commands I currently support are:
             [
                 [
                     InlineKeyboardButton(
-                        "Add ʋɛռօʍ ʍʊֆɨƈ ɮօȶ to your group", url="https://t.me/R2K_VENOM"
+                        "Add ʋɛռօʍ ʍʊֆɨƈ ɮօȶ to your group", url="t.me/{}?startgroup=true".format(context.bot.username)
                     )
                 ],
                 [
